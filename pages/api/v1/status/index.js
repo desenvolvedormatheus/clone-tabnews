3442869496de0a486f09ab32f1041c5ce567045f
Matheus Pereira de Souza <109758729+desenvolvedormatheus@users.noreply.github.com>
2025-07-21 18:42:07 +0000

    Adicionar testes e resposta do endpoint status da API

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,41 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json(
-    { chave: "Curso.dev muito bãun" }
-  )
+
+  // Pegar a data atual no formato ISO 8601
+  const updatedAt = new Date().toISOString();
+
+  // Pegar a versão do banco de dados 
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  // Pegar o maximo de conexões do banco de dados
+  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+
+  // pegar as conexões ativas
+  const databaseName = process.env.POSTGRES_DB;
+  const activeConnectionsResult = await database.query({
+    text: `
+        SELECT count(*)::int 
+        FROM pg_stat_activity 
+        WHERE datname = $1;
+        `,
+    values: [databaseName],
+  },
+  );
+  const activeConnectionsValue = activeConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        active_connections: activeConnectionsValue,
+      }
+    }
+  });
 }
 
 export default status;
--- tests/integration/api/status/get.test.js
+++ tests/integration/api/status/get.test.js
@@ -1,4 +1,37 @@
 test("Get deve retornar status 200", async () => {
-  const response = await fetch("http://localhost:3001/api/v1/status");
+  const response = await getResponseApi(body = false, version = 1, endpoint = "status");
   expect(response.status).toBe(200);
 });
+
+test("updated_at deve retornar uma data válida", async () => {
+  const responseBody = await getResponseApi(body = true, version = 1, endpoint = "status");
+  expect(responseBody.updated_at).toBeDefined();
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+});
+
+test("version do banco de dados deve ser uma string especifica", async () => {
+  const responseBody = await getResponseApi(body = true, version = 1, endpoint = "status");
+  const dbVersion = "16.0";
+  expect(responseBody.dependencies.database.version).toEqual(dbVersion);
+});
+
+test("Máximo de conexões deve ser 100", async () => {
+  const responseBody = await getResponseApi(body = true, version = 1, endpoint = "status");
+  const dbMaxConnections = 100;
+  expect(responseBody.dependencies.database.max_connections).toEqual(dbMaxConnections);
+});
+
+test("Conexões abertas deve ser 1", async () => {
+  const responseBody = await getResponseApi(body = true, version = 1, endpoint = "status");
+  const activeConnections = 1;
+  expect(responseBody.dependencies.database.active_connections).toEqual(activeConnections);
+});
+
+async function getResponseApi(body = false, version, enpoint, params = "") {
+  if (body) {
+    return (await fetch(`http://localhost:3000/api/v${version}/${enpoint}?${params}`)).json();
+  } else {
+    return await fetch(`http://localhost:3000/api/v${version}/${enpoint}?${params}`);
+  }
+}
