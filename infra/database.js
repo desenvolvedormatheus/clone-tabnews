dd6654398276d4b840381b4e044140e7cbc57a62
Matheus Pereira de Souza <109758729+desenvolvedormatheus@users.noreply.github.com>
2025-07-21 22:08:00 +0000

    Adição de dinâmica para aceitação de  em produção

--- infra/database.js
+++ infra/database.js
@@ -7,16 +7,10 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   })
 
   try {
-    console.log("Credenciais do postgres:", {
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT,
-      user: process.env.POSTGRES_USER,
-      database: process.env.POSTGRES_DB,
-      password: process.env.POSTGRES_PASSWORD,
-    });
     await client.connect()
     const result = await client.query(queryObject);
     return result;
